2d9c2cfd557ed81719d6e60ca4c511fa21b69f37
Geoffrey Jarman <geoff@Geoffreys-MacBook-Pro.local>
2022-04-06 14:02:07 -0500

    api fix

--- api.js
+++ api.js
@@ -2,5 +2,5 @@ export async function getWord() {
     return await fetch("https://six-hour-words.herokuapp.com/").then(res => res.json()).then(res => res.data);
 }
 export async function getList() {
-    return await fetch("https://six-hour-words.herokuapp.com/").then(res => res.json()).then(res => res.data);
+    return await fetch("https://six-hour-words.herokuapp.com/getList").then(res => res.json()).then(res => res.data);
 }
